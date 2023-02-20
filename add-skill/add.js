e88e60d143abbbef3fa3640ff196d7b4b64d1676
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-02-20 20:36:03 +0200

    send data function, add-skill button

--- add-skill/add.js
+++ add-skill/add.js
@@ -3,6 +3,33 @@ const viewSkillsBtn = document.querySelector("#view-skills-btn");
 const addSkillBtn = document.querySelector("#add-btn");
 const skillInput = document.querySelector("#skill");
 
+async function sendData() {
+    const skill = skillInput.value;
+    const dataToSend = {
+        skill: skill,
+    }
+
+    try {
+        const response = await fetch(BASE_URL + "/skills", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataToSend)
+        });
+        const result = await response.json();
+        alert("Success!");
+        window.location.href = "../index.html";
+    } catch {
+        alert("Error");
+    }
+}
+
+addSkillBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    sendData();
+})
+
 viewSkillsBtn.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "../index.html";
